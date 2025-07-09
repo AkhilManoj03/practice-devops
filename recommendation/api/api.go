516d6a5d2b361510c65f5ec65cf20dc009a83006
Akhil Manoj <manoja2@vcu.edu>
2025-07-09 19:55:24 -0400

    recomm: get products from db
    - Connected recommendation API with the postgres db to retrieve
    the products from there like the other services
    - Introduced database connection handling and product structure in
    the data package.
    - Replaced the origami selection logic with a new endpoint that
    fetches a random product from the database instead of fetching all
    products and selecting a random product from the full list

--- recommendation/api/api.go
+++ recommendation/api/api.go
@@ -2,24 +2,16 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"math/rand"
-	"recommendation/data"  // replace with your actual project name
-	"time"
+	"net/http"
+	"recommendation/data"
 )
 
 func GetOrigamiOfTheDay(c *gin.Context) {
-	origamis := data.GetDailyOrigami()
-	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
-	selectedOrigami := origamis[rand.Intn(len(origamis))] // get a random origami
-	
-	c.JSON(200, selectedOrigami)
-}
+	product, err := data.GetRandomProduct()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve a random product"})
+		return
+	}
 
-func StartAPI() {
-	r := gin.Default()
-	r.GET("/origami-of-the-day", GetOrigamiOfTheDay)
-
-	// TODO: Add more routes as needed
-	
-	r.Run(":8080")  // start the server on port 8080
+	c.JSON(http.StatusOK, product)
 }
